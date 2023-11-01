6f2e4d3dbdbc161c9f42062351222ca810adcb88
sjquant <seonujang92@gmail.com>
2023-11-02 00:30:34 +0900

    ✨ Add signup servcie

--- app/auth/service.py
+++ app/auth/service.py
@@ -2,6 +2,7 @@ import datetime
 import secrets
 
 import sqlalchemy as sa
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth.constants import ErrorEnum
@@ -14,6 +15,33 @@ from app.user.models import User
 class AuthService:
     def __init__(self, session: AsyncSession):
         self.session = session
+
+    async def sign_up_by_email(self, email: str, password: str, nickname: str):
+        user = User(
+            email=email,
+            nickname=nickname,
+        )
+        user.set_password(password)
+        self.session.add(user)
+
+        try:
+            await self.session.commit()
+        except IntegrityError as e:
+            if "duplicate" in str(e):
+                raise ValidationError(ErrorEnum.USER_ALREADY_EXISTS)
+            raise e
+
+        user.last_logged_in = datetime.datetime.now(tz=datetime.UTC)
+        refresh_token_model = RefreshToken(
+            user_id=user.id,
+            expires_at=datetime.datetime.now(tz=datetime.UTC)
+            + datetime.timedelta(seconds=config.refresh_token_expires_seconds),
+            token=secrets.token_urlsafe(32),
+        )
+        self.session.add(refresh_token_model)
+        await self.session.commit()
+
+        return user, refresh_token_model
 
     async def sign_in_by_email(self, email: str, password: str):
         res = await self.session.execute(sa.select(User).where(User.email == email))
